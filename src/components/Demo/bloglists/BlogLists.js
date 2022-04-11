2d39aa4844538f340e277ed4d454fec8aeb12c16
Ritik <multiplethings1557@gmail.com>
2022-04-11 20:15:25 +0530

    ADD: BLOG_Pages

--- src/components/Demo/bloglists/BlogLists.js
+++ src/components/Demo/bloglists/BlogLists.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import BlogSummary from '../blogsummary/BlogSummary'
 // styles
 import "./style.scss"
@@ -7,9 +8,17 @@ const BlogLists = ({ blogs }) => {
   return (
     <div className='card_container'>
       {
-        blogs && blogs.map(blog => <BlogSummary key={blog.id} blog={blog} />)
+        blogs.map((blog) => {
+          return (
+            <>
+              <Link to={"/blog/" + blog.id}>
+                <BlogSummary key={blog.id} blog={blog} />
+              </Link>
+            </>
+          )
+        })
       }
-
+      {/* blogs && blogs.map(blog => <> <Link to={`/blog/` + blog.id}><BlogSummary key={blog.id} blog={blog} /></Link></>) */}
     </div>
   )
 }
